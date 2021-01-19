9d34f0498daec3db6b710f671c9f817f266ccf88
PoojaDiya <77564572+PoojaDiya@users.noreply.github.com>
2021-01-19 15:04:12 +0400

    Update index.php

--- index.php
+++ index.php
@@ -43,13 +43,7 @@ session_start();
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Disabled</a>
+        <a class="nav-link" href="#">View Customer Details</a>
       </li>
     </ul>
   </div>
